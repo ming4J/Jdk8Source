ec8c7bd2b7a24d741382ed0cd038ac55ab2eb0eb
邵明 <shaoming@shaomingdeMBP.lan>
2020-03-28 00:26:25 +0800

    initCommited

---
+++ src/cn/shellming/test/Init.java
@@ -0,0 +1,17 @@
+package cn.shellming.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName Init
+ * @Description TODO
+ * @Author shaoming
+ * @Date 2020/3/28 12:21 上午
+ */
+public class Init {
+    public static void main(String[] args) {
+        Map<String,Integer> map = new HashMap<>();
+
+    }
+}
